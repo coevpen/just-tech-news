f1a73f3ce2cd4c1bdc76b07e635b0e7aab53ad4b
coevpen <koinuevins@gmail.com>
2022-01-30 19:54:31 -0600

    set up homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 // if request to endpoint doesn't exist, 404 error
